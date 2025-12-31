81abcfe8dd88ba6b75b17a18babf837524042cd5
haike0513 <haike0513@gmail.com>
2025-12-31 17:20:06 +0800

    feat: Change default theme from light to dark in layout component for improved user experience.

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
       <body className="antialiased">
         <ThemeProvider
           attribute="class"
-          defaultTheme="light"
+          defaultTheme="dark"
           enableSystem
           disableTransitionOnChange
         >
